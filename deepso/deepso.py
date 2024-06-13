d4eef35f3a036ec2f06c2ef63aeb427798f0072d
arthurvalls <arthur.valls5@gmail.com>
2024-06-13 17:44:38 -0300

    v4

--- deepso/deepso.py
+++ deepso/deepso.py
@@ -1,8 +1,8 @@
 import numpy as np
 from tqdm import tqdm
 import datetime
-import matplotlib.pyplot as plt
 import logging
+
 
 class Particle:
     def __init__(self, cost_function, num_dimensions, bounds, w, c1, c2, c3, F, CR, v_max):
@@ -18,32 +18,30 @@ class Particle:
         self.v_max = v_max
         self.position = np.random.uniform(bounds[0], bounds[1], num_dimensions)
         self.velocity = np.random.uniform(-self.v_max, self.v_max, num_dimensions)
+        self.score = self.cost_function(self.position)
         self.pbest_position = self.position.copy()
-        self.pbest_score = self.cost_function(self.position)
-        self.alpha = 0.2
+        self.pbest_score = self.score
 
-    def update(self, gbest_position, swarm):
-        
+    def update(self, gbest_position, swarm, it):
         indices = list(range(swarm.num_particles))
         indices.remove(swarm.particles.index(self))
-        a, b, c = np.random.choice(indices, 3, replace=False)
-        mutant = swarm.particles[a].position + self.F * (gbest_position - swarm.particles[c].position)
+        a, b = np.random.choice(indices, 2, replace=False)
+        mutant = swarm.particles[a].position + self.F * (gbest_position - swarm.particles[b].position)
         
         mutant = np.clip(mutant, self.bounds[0], self.bounds[1])
-        
-        # trial vec
-        self.position = np.where(np.random.uniform(size=self.num_dimensions) < self.CR, mutant, self.position) 
-        
+
+        # trial vector
+        self.position = np.where(np.random.uniform(size=self.num_dimensions) < self.CR, mutant, self.position)
+
         r1, r2, r3 = np.random.uniform(size=3)
         self.velocity = (self.w * self.velocity +
-                             self.c1 * r1 * (self.pbest_position - self.position) +
-                             self.c2 * r2 * (gbest_position - self.position) + 
-                             self.c3 * r3 * (gbest_position - self.pbest_position)) # delta weight
-        
+                         self.c1 * r1 * (self.pbest_position - self.position) +
+                         self.c2 * r2 * (gbest_position - self.position) + 
+                         self.c3 * r3 * (gbest_position - self.pbest_position))
+
         
         self.velocity = np.clip(self.velocity, -self.v_max, self.v_max)
         
-
         self.position = self.position + self.velocity
         self.position = np.clip(self.position, self.bounds[0], self.bounds[1])
         
@@ -51,34 +49,116 @@ class Particle:
 
         if score < self.pbest_score:
             self.pbest_score = score
-            self.pbest_position = self.position
-        
+            self.pbest_position = self.position.copy()
+        else:
+            self.position = np.where(np.random.uniform(size=self.num_dimensions) < self.CR, self.position, self.pbest_position)
+
         return score
 
 
 class Swarm:
-    def __init__(self, cost_function, num_particles, num_dimensions, num_iterations, bounds, w, c1, c2, c3, F, CR, v_max):
+    def __init__(self, cost_function, num_particles, num_dimensions, num_iterations, bounds, w, c1, c2, c3, F, CR, v_max, verb=False):
         self.num_particles = num_particles
         self.num_dimensions = num_dimensions
         self.num_iterations = num_iterations
+        self.cost_function = cost_function
         self.bounds = bounds
-        self.particles = [Particle(cost_function, num_dimensions, bounds, w, c1, c2, c3, F, CR, v_max) for _ in range(num_particles)]
+        self.w = w
+        self.c1 = c1
+        self.c2 = c2
+        self.c3 = c3
+        self.F = F
+        self.CR = CR
+        self.v_max = v_max
+        self.f_evals = 0
+        self.max_f_evals = 100_000
+        self.particles = [Particle(self.cost_function_eval, num_dimensions, bounds, w, c1, c2, c3, F, CR, v_max) for _ in range(num_particles)]
         self.gbest_position = min(self.particles, key=lambda p: p.pbest_score).pbest_position
         self.gbest_score = min(p.pbest_score for p in self.particles)
-    
+        self.verb = verb
+
+
+    class Result:
+        def __init__(self, gbest_score, gbest_position, f_evals, iters, bests):
+            self.F = gbest_score
+            self.X = gbest_position
+            self.evals = f_evals
+            self.max_iters = iters
+            self.history = bests
+
+
+    def tournament(self, tournament_size=2):
+        # Sort particles based on a suitable criterion (e.g., score)
+        selected_tournament = sorted(self.particles, key=lambda p: p.score)[:16]
+        selected_parents = []
+
+        # Ensure selected_tournament is a numpy array for np.random.choice
+        selected_tournament = np.array(selected_tournament)
+
+        for _ in range(len(selected_tournament)):
+            # Ensure tournament size is not larger than the number of selected_tournament
+            participants = np.random.choice(selected_tournament, tournament_size, replace=False)
+            
+            # Find the winner among the participants based on score
+            winner = min(participants, key=lambda p: p.score)
+            selected_parents.append(winner)
+        
+        return selected_parents
+
+    def update_swarm(self, offspring):
+        # Replace worst particles with offspring (or merge them)
+        combined_swarm = self.particles + offspring
+        combined_swarm.sort(key=lambda p: p.score)
+        self.particles = combined_swarm[:len(self.particles)]
+
+    def crosspoint(self, p1, p2):
+        # Example single-point crossover
+        child1 = Particle(self.cost_function_eval, self.num_dimensions, self.bounds, self.w, self.c1, self.c2, self.c3, self.F, self.CR, self.v_max)
+        pos = np.where(np.random.uniform(size=self.num_dimensions) < self.CR, p1, p2)
+        vel = np.mean([p1.velocity, p2.velocity], axis=0)
+        child1.position = pos
+        child1.velocity = vel
+        return child1
+
+    def crossover(self, selected_parents):
+        offspring = []
+        for i in range(0, len(selected_parents), 2):
+            parent1 = selected_parents[i]
+            parent2 = selected_parents[i + 1]
+            child1 = self.crosspoint(parent1, parent2)
+            offspring.append(child1)
+        return offspring
+
+    def cost_function_eval(self, x):
+        self.f_evals += 1
+        return self.cost_function(x)
+
     def optimize(self):
         bests = []
-        for iteration in tqdm(range(self.num_iterations)):
+        k = 0
+        if self.verb:
+            print(f'f_evals | gb')
+        while self.f_evals < self.max_f_evals:
             for particle in self.particles:
-                score = particle.update(self.gbest_position, self)
+                if self.f_evals >= self.max_f_evals:
+                    break
+                score = particle.update(self.gbest_position, self, k)
                 if score < self.gbest_score:
                     self.gbest_score = score
-                    self.gbest_position = particle.position
-            bests.append(self.gbest_score)
-        
-            # print(f"Global Best Score: {self.gbest_score}")
-            # print(f"Global Best Position: {self.gbest_position}")
-        return self.gbest_score, self.gbest_position, bests
+                    self.gbest_position = particle.position.copy()
+            # parents = self.tournament()
+            # children = self.crossover(parents)
+            # self.update_swarm(children)
+            k += 1
+            if self.verb:
+                if self.f_evals % 100 == 0:
+                    print(f'{self.f_evals}| {self.gbest_score}')
+            if self.verb:
+                print(f'X: {self.gbest_position}')
+                print(f'F: {self.gbest_score}')
+
+        return self.Result(self.gbest_score, self.gbest_position, self.f_evals, k, bests)
+
 
     def optimize_params(self, logger):
         print("Params optimization starting...")
@@ -88,17 +168,18 @@ class Swarm:
             is_improved = False
             
             for particle in self.particles:
-                score = particle.update(self.gbest_position, self)
+                if self.f_evals >= self.max_f_evals:
+                    return self.gbest_score, self.gbest_position
+                score = particle.update(self.gbest_position, self, iteration)
                 if score < self.gbest_score:
                     self.gbest_score = score
-                    self.gbest_position = particle.position
+                    self.gbest_position = particle.position.copy()
                     is_improved = True
+
             
             self.print_info(is_improved, iteration, logger)
-                
         
         return self.gbest_score, self.gbest_position
-
 
     def print_info(self, is_improved, iteration, logger):
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -115,28 +196,26 @@ class Swarm:
             log_message.append(f"Best score: {self.gbest_score}")
             log_message.append(f"Best params: {self.gbest_position}")
 
-        # Join the log messages into a single string and log it
         full_message = "\n".join(log_message)
         logger.info(full_message)
-
-        # Optionally, also print to console if desired
         print(full_message)
 
 
 class DEEPSO:
-    def __init__(self, cost_function, num_dimensions=2, num_particles=30, num_iterations=100, w=0.5, c1=2.0, c2=2.0, c3=0.85,F=0.5, CR=0.9, v_max=1, bounds=(-2.048, 2.048)):
-        self.swarm = Swarm(cost_function, num_particles, num_dimensions, num_iterations, bounds, w, c1, c2, c3, F, CR, v_max)
+    def __init__(self, cost_function, num_dimensions=2, num_particles=30, num_iterations=100, w=0.5, c1=2.0, c2=2.0, c3=0.85, F=0.5, CR=0.9, v_max=1, bounds=(-2.048, 2.048), verb=False):
+        self.swarm = Swarm(cost_function, num_particles, num_dimensions, num_iterations, bounds, w, c1, c2, c3, F, CR, v_max, verb)
 
     def run(self):
+        print("Minimizing...")
+        print(f"Max # of function evaluations: {self.swarm.max_f_evals}")
         return self.swarm.optimize()
 
     def optimize_params(self):
         current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        # Configure logging
         logging.basicConfig(
-            filename=f'logs/results{current_time}.log',        # Name of the log file
-            level=logging.INFO,           # Set the logging level to INFO
-            format='%(message)s'          # Use a simple format for the log messages
+            filename=f'logs/results{current_time}.log',
+            level=logging.INFO,
+            format='%(message)s'
         )
-        logger = logging.getLogger(__name__)  # Get the logger object
+        logger = logging.getLogger(__name__)
         return self.swarm.optimize_params(logger)
--- deepso/main.py
+++ deepso/main.py
@@ -1,20 +1,78 @@
 from deepso import DEEPSO
 import numpy as np
 from tqdm import tqdm
-import deepso2
+# import deepso2
+
 
 def rosenbrock(x, a=1, b=100):
     return sum((a-x[i])**2 + b*(x[i+1]-x[i]**2)**2 for i in range(len(x)-1))
 
+def rastrigin(x, a=10):
+    return a * len(x) + np.sum(x**2 - a * np.cos(2 * np.pi * x))
 
-# def optimize_deepso(params):
-#     w, c1, c2, F, CR, v_max = params
-#     num_dimensions = 30
-#     num_particles = 30
-#     num_iterations = 100
-#     deepso = DEEPSO(rosenbrock, num_dimensions, num_particles, num_iterations, w, c1, c2, F, CR, v_max, bounds=(-2.048, 2.048))
-#     cost, _ = deepso.run()
-#     return cost
+def optimize_deepso(params):
+    w, c1, c2, c3, F, CR, v_max = params
+    num_dimensions = 30
+    num_particles = 30
+    num_iterations = 100
+    deepso = DEEPSO(rosenbrock, num_dimensions, num_particles, num_iterations, w, c1, c2, c3, F, CR, v_max, bounds=(-2.048, 2.048))
+    cost, _, _ = deepso.run()
+    return cost
+
+def run_rounds():
+    w,c1,c2,c3,F,CR,v_max = [0.31419906, 1.47620838, 1.2800813, 0.85, 0.36238664, 0.56160154, 0.35317888] #21 +avg -std
+    dim = 30
+    particles = dim if dim > 30 else 30
+    iters = int(np.round(100_000/particles))
+
+    best_vec = []
+    for _ in tqdm(range(30)):
+        deepso = DEEPSO(rosenbrock, dim, particles, iters, w, c1, c2, c3, F, CR, v_max, bounds=(-2.048, 2.048))
+        res = deepso.run()
+        print(f'Best parameters: {res.X}')
+        print(f'Best cost: {res.F}')
+        best_vec.append(res.F)
+
+    print('30 dim:')
+    print(f'avg: {np.mean(best_vec)}')
+    print(f'std-dv: {np.std(best_vec)}')
+
+    w,c1,c2,c3,F,CR,v_max = [0.31419906, 1.47620838, 1.2800813, 0.85, 0.36238664, 0.56160154, 0.35317888] #21 +avg -std
+
+    dim = 50
+    particles = dim if dim > 30 else 30
+    iters = int(np.round(100_000/particles))
+
+
+    best_vec = []
+    for _ in tqdm(range(30)):
+        deepso = DEEPSO(rosenbrock, dim, particles, iters, w, c1, c2, c3, F, CR, v_max, bounds=(-2.048, 2.048))
+        res = deepso.run()
+        print(f'Best parameters: {res.X}')
+        print(f'Best cost: {res.F}')
+        best_vec.append(res.F)
+
+    print('50 dim')
+    print(f'avg: {np.mean(best_vec)}')
+    print(f'std-dv: {np.std(best_vec)}')
+
+    w,c1,c2,c3,F,CR,v_max = [0.31419906, 1.47620838, 1.2800813, 0.85, 0.36238664, 0.56160154, 0.35317888] #21 +avg -std
+
+    dim = 100
+    particles = dim if dim > 30 else 30
+    iters = int(np.round(100_000/particles))
+
+    best_vec = []
+    for _ in tqdm(range(30)):
+        deepso = DEEPSO(rosenbrock, dim, particles, iters, w, c1, c2, c3, F, CR, v_max, bounds=(-2.048, 2.048))
+        res = deepso.run()
+        print(f'Best parameters: {res.X}')
+        print(f'Best cost: {res.F}')
+        best_vec.append(res.F)
+
+    print('100 dim')
+    print(f'avg: {np.mean(best_vec)}')
+    print(f'std-dv: {np.std(best_vec)}')
 
 
 # def optimize_deepso2(params):
@@ -30,25 +88,18 @@ def rosenbrock(x, a=1, b=100):
 #     cost, _ = deepso.run()
 #     return cost
 
-# best for now
-w,c1,c2,c3,F,CR,v_max = [0.31419906, 1.47620838, 1.2800813, 0.85, 0.36238664, 0.56160154, 0.35317888] #21 +avg -std
 
-dim = 30
-particles = dim
-iters = int(np.round(100_000/particles))
+def run_single():
+    # best for now
+    w,c1,c2,c3,F,CR,v_max = [0.31419906, 1.47620838, 1.2800813, 0.85, 0.36238664, 0.56160154, 0.35317888] #21 +avg -std
+    # w,c1,c2,c3,F,CR,v_max= np.array([ 0.35507922, -0.57976092, 1.60272123, -0.84557449, 0.8177705, 0.32162406, 1.65246025]) 
+    dim = 30
+    particles = dim if dim > 30 else 30
+    iters = 10
+    deepso = DEEPSO(rosenbrock, dim, particles, iters, w, c1, c2, c3, F, CR, v_max, (-2.048, 2.048))
+    res = deepso.run()
+    print(f'Best parameters: {res.X}')
+    print(f'Best cost: {res.F}')
 
-best_vec = []
-for _ in tqdm(range(30)):
-    deepso = DEEPSO(rosenbrock, dim, particles, iters, w, c1, c2, c3, F, CR, v_max, bounds=(-2.048, 2.048))
-    best_cost, best_params, _ = deepso.run()
-    print(f'Best parameters: {best_params}')
-    print(f'Best cost: {best_cost}')
-    best_vec.append(best_cost)
-
-
-print(f'avg: {np.mean(best_vec)}')
-print(f'std-dv: {np.std(best_vec)}')
-# deepso = DEEPSO(rosenbrock, dim, particles, iters, w, c1, c2, c3, F, CR, v_max, bounds=(-2.048, 2.048))
-# best_cost, best_params, _ = deepso.run()
-# print(f'Best parameters: {best_params}')
-# print(f'Best cost: {best_cost}')
+run_single()
+# run_rounds()
